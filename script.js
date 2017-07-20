a3e2f788d979e8e785e920f77b4fe1163c5e4b06
Hieratikos <jordicombs@yahoo.com>
2017-07-20 16:25:37 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Admin on 5/11/2017.
+ */
+$(document).ready(function () {
+    $("#myImgQuake").click(function () {
+        $("#myModal")[0].style.display = "block";
+        $("#imgContent")[0].src = $("#myImgQuake")[0].src;
+        $("#imgCaption").html($("#myImgQuake")[0].alt);
+    });
+    $("#myImgAlg").click(function () {
+        $("#myModal")[0].style.display = "block";
+        $("#imgContent")[0].src = $("#myImgAlg")[0].src;
+        $("#imgCaption").html($("#myImgAlg")[0].alt);
+    });
+    $(".fa-close").click(function () {
+        $("#myModal")[0].style.display = "none";
+    });
+    //close the modal with the escape key
+    document.addEventListener('keyup', function(e) {
+        if (e.keyCode === 27) {
+            $("#myModal")[0].style.display = "none";
+        }
+    });
+});
+//email submit
+function confirmMsg() {
+    alert("Thank you for your interest. This page is under construction.");
+    location.href = location.pathname;
+    location.href = "#";
+}
